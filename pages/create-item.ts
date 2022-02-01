1e84ca6f2746c695288a47de1f3ce60c8d411f8e
Douglas <55341891+menza-bot@users.noreply.github.com>
2022-02-01 10:45:49 +0300

    first commit

---
+++ pages/create-item.ts
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { ethers } from 'ethers'
+import { create as ipfsHttpClient } from 'ipfs-http-client'
+import { useRouter } from 'next/router'
+import Web3Modal from 'web3modal'
+
+interface SyntheticEvent {
+    bubbles: boolean;
+    cancelable: boolean;
+    currentTarget: EventTarget;
+    defaultPrevented: boolean;
+    eventPhase: number;
+    isTrusted: boolean;
+    nativeEvent: Event;
+    preventDefault(): void;
+    stopPropagation(): void;
+    target: EventTarget;
+    timeStamp: Date;
+    type: string;
+}
+
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import Web3Modal from "web3modal"
+
+export const Home = () => {
+  return (
+    <div>
+      Open sea
+    </div>
+  )
+}
